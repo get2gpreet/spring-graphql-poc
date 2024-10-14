6056df57da7aacdddf7c32ca893184967a7895b4
get2gpreet <get2gpreet@gmail.com>
2024-10-14 12:47:42 -0400

    First commit

---
+++ src/main/java/com/codie/SpringGraphqlPocApplication.java
@@ -0,0 +1,13 @@
+package com.codie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codie/SpringGraphqlPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class SpringGraphqlPocApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
